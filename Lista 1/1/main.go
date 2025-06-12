0db84b6d2c0f7b5b23a37b19656cf36882823486
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-06-12 20:15:19 -0300

    exercicio 1 e 2

---
+++ Lista 1/1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var min int
+	var max int
+	fmt.Println("Digite a quantidade minima:")
+	fmt.Scan(&min)
+	fmt.Println("Digite a quantidade maxima:")
+	fmt.Scan(&max)
+	resultado := estoqueMedio(min,max)
+	fmt.Println("O estoque medio e: ", resultado)
+}
+
+func estoqueMedio(min, max int) int{
+	estoque := (min + max) / 2
+	return estoque
+}

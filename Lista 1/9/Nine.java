62990ddd7cf16a5a527ff9328b72d85c3da70a70
Carlossnogueira <carlosnogueira372@gmail.com>
2025-10-20 09:20:21 -0300

    exercise

---
+++ Lista 1/9/Nine.java
@@ -0,0 +1,25 @@
+/*
+ * Faça  um  algoritmo  que  leia  a  idade  de  uma  pessoa  expressa  em  anos,  meses  e  dias  e  escreva  a  idade  dessa 
+ * pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias
+ */
+
+import java.util.Scanner;
+
+public class Nine {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+           
+            System.out.println("Day?");
+            int day = sc.nextInt();
+
+            System.out.println("Mouth?");
+            int month = sc.nextInt();
+
+            System.out.println("Year?");
+            int years = sc.nextInt();
+
+            int result = (years * 365) + (month * 30) + day;
+            System.out.printf("Your have: %d, days of life", result);
+        }
+    }
+}

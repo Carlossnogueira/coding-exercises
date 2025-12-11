1adc31d4dd10e2c28d2040e6275c775963912210
Carlossnogueira <carlosnogueira372@gmail.com>
2025-12-10 21:44:46 -0300

    exercise

---
+++ Lista 1/17/17.py
@@ -0,0 +1,12 @@
+"""
+Escreva um programa que leia um número inteiro. Verificar por meio de condição se o valor fornecido está na faixa 
+entre  0  (zero)  e  9  (nove).  Caso  o  valor  fornecido  esteja  dentro  da  faixa,  apresentar  a  mensagem  “valor  válido”. 
+Caso contrário, apresentar a mensagem “valor inválido”.
+"""
+
+value = int(input("Value: "))
+
+if value >= 0 and value <= 9:
+    print("Valid Value")
+else:
+    print("Invalid value")

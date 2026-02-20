ccb363a7c10b5ac5120f4ff891c0dca3fd59382d
Carlossnogueira <carlosnogueira372@gmail.com>
2026-02-20 19:25:39 -0300

    exercise

---
+++ List 3/data/list/2.cs
@@ -0,0 +1,8 @@
+/*
+Dada uma List com alguns nomes, imprima o primeiro e o último elemento usando indexação
+*/
+
+List<string> names = new List<string>(){"Juan","Maria","Lucia"};
+
+Console.WriteLine(names[0]);
+Console.WriteLine(names[names.Count - 1]);

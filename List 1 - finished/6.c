0089d678104bd67e194f72dbf3335f6294225b4e
Carlossnogueira <carlosnogueira372@gmail.com>
2026-03-01 17:54:53 -0300

    exercise

---
+++ List 1 - finished/6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    float C , F;
+
+    printf("Digite a temperatura em Celsius: ");
+    scanf("%f", &C);
+
+    F = (9 * C + 160) / 5;
+    printf("A temperatura em Fahrenheit é: %.2f\n", F);
+
+    return 0;
+}
